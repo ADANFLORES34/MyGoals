3fed84350d111a30797ebe1959ed307fd75a296f
ADANFLORES34 <adanflores0506@gmail.com>
2024-06-05 14:29:57 -0700

    1er cambio

--- App.js
+++ App.js
@@ -1,97 +1,54 @@
-import { useState } from 'react'
-import { Button, StyleSheet, Text, TextInput, View, ScrollView, FlatList } from 'react-native';
+import { StyleSheet } from 'react-native'
+import CategoriesScreen from "./screens/CategoriesScreens";
+import { StatusBar } from 'expo-status-bar';
 
-import GoalItem from './Components/GoalItem';
-import GoalInput from './Components/GoalInput'
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack'
+import MealsOverviewScreen from './screens/MealsOverviewScreen';
+import MealDetailScreen from './screens/MealDetailScreen';
+
+const Stack = createStackNavigator();
 
 export default function App() {
-  const [goals, setGoals] = useState([])
-
-
-  function handleAddGoal(enteredGoalText) {
-    // console.log(enteredGoalText)
-    // console.log('Hello You')
-    setGoals(() => [...goals, {text: enteredGoalText, key: Math.random().toString()}])
-    console.log('goals', goals)
-    console.log('handleAddGoal')
-  }
-
-  function handleDeleteGoal(id){
-    console.log('DELETE')
-    const deleteGoal = goals.filter((goal) => {return goal.key !== id} )
-    setGoals(deleteGoal)
-  }
 
   
   return (
-    <View style={styles.container}>
-      {/* <View style={styles.inputContainer}>
-        <TextInput 
-          style={styles.textInput} 
-          placeholder='Your Goal!'
-          onChangeText={handleInputGoal}
-        />
-        <Button 
-          title="Add Goal" 
-          color={'#A3FFD6'}
-          onPress={handleAddGoal}
-        />
-      </View> */}
-      <GoalInput
-        onAddGoal={handleAddGoal}
-      />
-      <View style={styles.goalsContainer}>
-        <FlatList
-          data={goals}
-          renderItem={ (itemData) => {
-            return(
-              // <View style={styles.goalsItem} >
-              //   <Text style={styles.goalText}>{itemData.item.text}</Text>
-              // </View>
-              <GoalItem 
-                itemData={itemData}
-                onDeleteItem={handleDeleteGoal} 
-                id={itemData.item.key}
-              />
-            )
+    <>
+      <StatusBar style='dark' />
+      <NavigationContainer>
+        <Stack.Navigator
+          screenOptions={{
+              headerStyle: { backgroundColor: '#987070'},
+              headerTintColor: 'white',
+              contentStyle: { backgroundColor: '#F1E5D1'}
           }}
-          keyExtractor={(item) => {
-            return item.id
-          }}
-        />
-      </View>
-    </View>
+        >
+          <Stack.Screen 
+            name="MealsCategories" 
+            component={CategoriesScreen}
+          />
+          <Stack.Screen 
+            name="MealsOverview" 
+            component={MealsOverviewScreen}
+            options={{
+              title: 'All categories',
+              headerTintColor: 'white',
+              contentStyle: { backgroundColor: '#C39898'}
+            }} 
+          />
+          <Stack.Screen 
+            name="MealDetail" 
+            component={MealDetailScreen} 
+          />
+
+        </Stack.Navigator>
+      </NavigationContainer>
+    </>
+
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    paddingTop: 50,
-    paddingHorizontal: 20,
-  },
-  inputContainer: {
-    flex: 1,
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-    alignItems: 'center',
-    marginBottom: 20,
-    borderBottomWidth: 1,
-    borderBottomColor: '#7BC9FF'
-  },
-  btnGoal:{
-    borderRadius: 20,
-    backgroundColor: '#cccccc'
-  },
-  textInput: {
-    borderWidth: 1,
-    borderColor: '#cccccc',
-    width: '80%',
-    marginRight: 3,
-    padding: 8,
-    borderRadius: 5
-  },
-  goalsContainer: {
-    flex: 5
-  },
-});
+  }
+})
